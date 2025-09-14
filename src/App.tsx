1fed418e6479d97bb00a9fd03dfde991b7aef426
nmhieu32 <nmhieutt2003@gmail.com>
2025-09-14 15:43:27 +0700

    fix: update Toaster import path and format return statement in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { useRoutes } from "react-router-dom";
 import "./App.css";
 import { routes } from "./routes";
-import { Toaster } from "sonner";
+import { Toaster } from "./components/ui/sonner";
+
 
 function App() {
   const generateRoute = useRoutes(routes);
-  return <>
-  <Toaster position="top-center" richColors/>
-  {generateRoute}</>;
+  return (
+    <>
+      <Toaster position="top-center" richColors/>
+      {generateRoute}
+    </>
+  );
 }
 
 export default App;
